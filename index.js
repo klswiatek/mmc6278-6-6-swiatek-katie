c4cc321fd08c4cec04828ceb1b070787c32b9474
klswiatek <klswiatek@yahoo.com>
2022-06-20 09:48:42 -0400

    Event listeners added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const toggleButton = document.getElementsByClassName('hamburger-btn')[0]
 const navbarLinks = document.getElementsByClassName ('hamburger-menu')[0]
 
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
